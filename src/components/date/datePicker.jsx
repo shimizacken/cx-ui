9bc23a47ae4ff7dee1e5c57211077c7c06135eab
Shimi Zacken <shimizacken@gmail.com>
2018-02-06 22:23:58 +0200

    modules.js added

---
+++ src/components/date/datePicker.jsx
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+
+export default class DatePicker extends PureComponent {
+
+    render() {
+
+        return(
+            null
+        );
+    }
+}
---
+++ src/modules.js
@@ -0,0 +1,7 @@
+//export Button from './js/Autocompletes/Autocomplete';
+//export TreeView from './js/Avatars/Avatar';
+//export DatePicker from './DatePicker/DatePicker';
+//export SideBar from '';
+
+import DatePicker from './components/date/datePicker';
+export DatePicker;
